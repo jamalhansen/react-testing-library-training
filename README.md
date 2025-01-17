4a9a4507216bd076588cecfd317cbb7e865320b9
Jamal Hansen <jamal.hansen@gmail.com>
2025-01-17 06:59:02 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Testing Library Training
+
+This git repository is for code associated with the React and Testing Library training available at the link below
+
+<https://udemy.com/course/react-testing-library-and-jest/>
---
+++ users/src/UserList.js
@@ -0,0 +1,20 @@
+export default function UserList({ users }) {
+  const renderedUsers = users.map((user) => {
+    return (
+      <tr key={user.name}>
+        <td>{user.name}</td>
+        <td>{user.email}</td>
+      </tr>
+    );
+  });
+
+  return (
+    <table>
+      <thead>
+        <th>Name</th>
+        <th>Email</th>
+      </thead>
+      <tbody>{renderedUsers}</tbody>
+    </table>
+  );
+}
